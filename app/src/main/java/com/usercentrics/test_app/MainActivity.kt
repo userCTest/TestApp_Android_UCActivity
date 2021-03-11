33f7c8fd133b20453f1fdebfc34aac02c71976e1
userCTest <rui.reis@usercentrics.com>
2021-03-11 12:12:43 +0000

    Upgrade to 1.10.0

--- app/src/main/java/com/usercentrics/test_app/MainActivity.kt
+++ app/src/main/java/com/usercentrics/test_app/MainActivity.kt
@@ -4,26 +4,24 @@ import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import android.view.View
-import androidx.appcompat.app.ActionBar
 import androidx.appcompat.app.AppCompatActivity
 import com.usercentrics.sdk.UsercentricsActivity
 import com.usercentrics.sdk.models.common.UserOptions
 
 
 class MainActivity : AppCompatActivity() {
+
+    // setingsIds: cmp v1 - egLMgjg9j
+    //             cmp v2 - Insi1P6_p
+    //                      3NdtofNZ6
+    //                      eE6nmpe4v
+    //                      tbdaymyIU
+    private val settingsId = "Insi1P6_p"
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        // setingsIds: cmp v1 - egLMgjg9j
-        //             cmp v2 - Insi1P6_p
-        //                      3NdtofNZ6
-        //                      eE6nmpe4v
-        //                      tbdaymyIU
-        val settingsId = "o_i8tHKTp"
-
-        val bar: android.app.ActionBar? = getActionBar()
-        bar?.setTitle("SettingsId - $settingsId")
 
         val userOptions = UserOptions(
             controllerId = null,
