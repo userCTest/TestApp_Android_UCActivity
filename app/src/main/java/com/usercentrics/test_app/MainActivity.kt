97a1495adbee2cf40ba611c89fb540bead8702cc
userCTest <rui.reis@usercentrics.com>
2021-05-11 09:01:27 +0100

    Upgrade SDK v1.11.1

--- app/src/main/java/com/usercentrics/test_app/MainActivity.kt
+++ app/src/main/java/com/usercentrics/test_app/MainActivity.kt
@@ -1,30 +1,26 @@
-package com.ruimgreis.test_app
+package com.ruimgreis.test_app_ucactivity
 
-import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import android.view.View
-import android.view.View.OnFocusChangeListener
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.usercentrics.sdk.*
 import com.usercentrics.sdk.models.common.*
+import com.usercentrics.sdk.ui.PredefinedUISettings
 import kotlinx.android.synthetic.main.activity_main.*
 
 
 class MainActivity : AppCompatActivity() {
-
-    // setingsIds: cmp v1 - egLMgjg9j
-    //             cmp v2 - Insi1P6_p
-    //                      3NdtofNZ6
-    //                      eE6nmpe4v
-    //                      tbdaymyIU
-    //             tcf2   - opn4EGC0S
-    //             tcf2   - ZDQes7xES
-    //             tcf2   - EA4jnNPb9
-    //             tcf2   - hKTmJ4UVL
-    //             ccpa   - syV5G8hMG
+    // var to contain insjected settingsId
     private lateinit var settingsId: String
+    // val to contain the activity launcher, to be used to start the UsercentricsActivityLauncher in method
+    // showCMP
+    private val usercentricsActivityLauncher =
+        registerForActivityResult(UsercentricsActivityContract()) { services ->
+            Log.d(MainActivity::class.simpleName, "registerForActivityResult services: $services")
+        }
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -32,44 +28,40 @@ class MainActivity : AppCompatActivity() {
 
         // hides soft input keyboard
         input_settings_id.showSoftInputOnFocus = false
-
+        // version input
         txt_version.text =  getVersionName()
-
+        // go button input to get settingsId
         btn_go.setOnClickListener(){ _ ->
             if(input_settings_id.text.toString().isNullOrEmpty()){
                 Toast.makeText(this,
                     "Please insert a SettingsId",
                     Toast.LENGTH_LONG).show()
             } else {
-                settingsId = input_settings_id.text.toString() ?: settingsId
+                settingsId = input_settings_id.text.toString().trim()
+                // Launch CMP
                 showCMP(settingsId)
             }
         }
 
+        // btn close app
         val btn_close_app= findViewById<View>(R.id.btn_close_app)
         btn_close_app.setOnClickListener{
             this.finishAffinity()
         }
     }
 
+    /**
+     * gets version of SDK being used
+     */
     private fun getVersionName(): String {
         val pInfo = this.getPackageManager().getPackageInfo(this.getPackageName(), 0)
         return pInfo.versionName ?: "test"
-}
-
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        super.onActivityResult(requestCode, resultCode, data)
-        if (requestCode == UsercentricsActivity.REQUEST_CODE &&
-            resultCode == UsercentricsActivity.RESULT_OK_CODE
-        ) {
-            val services = UsercentricsActivity.getResult(data)
-            for (service in services) {
-                Log.i("Consents: ", service.toString());
-            }
-        }
     }
 
+    // shows CMP, using UsercentricsActivityLauncher
+    // https://docs.usercentrics.com/cmp_in_app_sdk/latest/predefined_ui/present/#presenting-the-cmp
     private fun showCMP(settingsId: String) {
+
         val userOptions = UserOptions(
             controllerId = null,
             defaultLanguage = null,
@@ -80,6 +72,17 @@ class MainActivity : AppCompatActivity() {
             noCache = null
         )
 
-        UsercentricsActivity.start(this, settingsId, userOptions)
+        val predefinedUISettings = PredefinedUISettings(
+            showCloseButton = false,
+//            customFont = customFont
+        )
+
+        val arguments = UsercentricsActivityArguments(
+            settingsId = settingsId,
+            userOptions = userOptions,
+            predefinedUISettings = predefinedUISettings
+        )
+
+        usercentricsActivityLauncher.launch(arguments)
     }
 }
